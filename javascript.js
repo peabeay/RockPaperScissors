d659c3ad207b90624849b2f101c09cd719cc4ae6
Paul Bentley-Angell <167089809+peabeay@users.noreply.github.com>
2024-05-05 13:34:15 +0200

    Use Math.floor to find random number for Computer Guess

---
+++ javascript.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+
+let compguess = Math.floor(Math.random() * (3 - 1 +1)) + 1;
+console.log(compguess);
+
+
+
+// use random number generation to pick a number between 1 and 3
+// if number returned is 1, then print rock. etc.
