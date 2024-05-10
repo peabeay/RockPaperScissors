1e6c8b7f78c45df42d10cc42da3027cb2dd8e2d5
Paul Bentley-Angell <167089809+peabeay@users.noreply.github.com>
2024-05-10 12:16:48 +0200

    Make score variables increase following a win

--- javascript.js
+++ javascript.js
@@ -1,39 +1,80 @@
-
 let humanScore = 0;
-let computerScore = 0;
+let computerScore = 0; 
 
+// obtain computer choice or Rock Paper or Scissors
 function getComputerChoice() {
     const compGuess = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    console.log(compGuess);
     if (compGuess === 1) {
-        return "Rock";
+        return ("rock");
     } else if (compGuess === 2) {
-        return "Paper";
+        return ("paper");
     } else {
-        return "Scissors";
+        return ("scissors");
     }
 }
-console.log(getComputerChoice())
 
+// take input from user of their choice Rock Paper or Scissors
+let humanGuess = prompt("Rock, Paper or Scissors - You must choose!");
+
+// parse user selection to lowercase to allow for case insensitivity in user input
 function getHumanChoice() {
-    let humanGuess = prompt("Rock, Paper or Scissors - You must choose!");
-        if (humanGuess.toLowerCase() === "rock") {
-            return "Rock";
-        } else if (humanGuess.toLowerCase() === "paper") {
-            return "Paper";
-        } else if (humanGuess.toLowerCase() === "scissors") {
-            return "Scissors";
-        } else {
-            alert("You didn't enter Rock, Paper or Scissors so you can't play");
-        }
+    if (humanGuess.toLowerCase() === "rock") {
+        return ("rock");
+    } else if (humanGuess.toLowerCase() === "paper") {
+        return ("paper");
+    } else if (humanGuess.toLowerCase() === "scissors") {
+        return ("scissors");
+    } else {
+        alert("You didn't enter Rock, Paper or Scissors so you can't play");
     }
+}
+// console.log(getHumanChoice())
 
-console.log(getHumanChoice())
+// compare the results of computer and human choice to determine winner
 
-function playRound(humanChoice, computerGuess) {
-    
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return ("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++
+        return ("You lose! Paper Beats Rock");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++
+        return ("You win! Scissors beats Paper");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++
+        return ("You win! Rock Beats Scissors");
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++
+        return ("You lose! Rock Beats Scissors");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++
+        return ("You win! Paper Beats Rock");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++
+        return ("You lose! Scissors beats Paper");
+    } else {
+        return ("Try again");
+    }
 }
 
-// use random number generation to pick a number between 1 and 3
-// if number returned is 1, then print rock. etc.
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+const result = playRound(humanSelection, computerSelection);
+console.log(result);
 
-// Prompt for human choice of "Rock, Paper or Scissors"
+// keep scores after a round 
+
+
+
+// function keepScore() {
+//     if (result.includes("You win")) {
+//         let humanScore = humanScore++;
+//     } else if (result.includes("You lose")) {
+//         let computerScore = computerScore++;
+//     }
+// }
+// keepScore();
+console.log(humanScore);
+console.log(computerScore);
